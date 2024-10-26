155b99c081615db3c590d41e8860e4ef2b4b65c9
Tamim Mahmud Khan 　( タミム・マフムド・カン) <30326616+tamimmahmudkhan@users.noreply.github.com>
2024-10-26 21:09:34 +0900

    Update start.sh
    Improved detach chance by removing more kernel modules..

--- start.sh
+++ start.sh
@@ -4,6 +4,12 @@ set -x
 
 # Stop display manager
 systemctl stop display-manager.service
+systemctl stop sddm.service
+pulse_pid=$(pgrep -u igneel pulseaudio)
+pipewire_pid=$(pgrep -u igneel pipewire-media)
+kill $pulse_pid
+kill $pipewire_pid
+
 ## Uncomment the following line if you use GDM
 #killall gdm-x-session
 
@@ -17,9 +23,19 @@ echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 # Avoid a Race condition by waiting 2 seconds. This can be calibrated to be shorter or longer if required for your system
 sleep 2
 
+modprobe -r nvidia-drm
+modprobe -r nvidia-uvm
+modprobe -r snd_hda_intel
+modprobe -r i2c_nvidia_gpu
+modprobe -r nvidia
+
+sleep 2
+
 # Unbind the GPU from display driver
 virsh nodedev-detach pci_0000_01_00_0  #Replace numbers with your specific pci id. Use lspci -nnk
 virsh nodedev-detach pci_0000_01_00_1  # This one too
 
 # Load VFIO Kernel Module  
 modprobe vfio-pci  
+modprobe vfio
+modprobe vfio_iommu_type1
