8f4dc46c1abf0aa7655056462b6b1b5f3924953d
Tamim Mahmud Khan 　( タミム・マフムド・カン) <30326616+tamimmahmudkhan@users.noreply.github.com>
2024-10-31 20:27:34 +0900

    Update setup-single-gpu-passthrough.sh
    Added sudo

--- setup-single-gpu-passthrough.sh
+++ setup-single-gpu-passthrough.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
- mkdir -p /etc/libvirt/hooks/qemu.d/win11/{prepare/begin,release/end}
+#!/bin/fish
+ sudo mkdir -p /etc/libvirt/hooks/qemu.d/win11/{prepare/begin,release/end}
 
- cp qemu /etc/libvirt/hooks/
- chmod +x /etc/libvirt/hooks/qemu
+ sudo cp qemu /etc/libvirt/hooks/
+ sudo chmod +x /etc/libvirt/hooks/qemu
 
- cp start.sh /etc/libvirt/hooks/qemu.d/win11/prepare/begin/
- cp revert.sh /etc/libvirt/hooks/qemu.d/win11/release/end/
+ sudo cp start.sh /etc/libvirt/hooks/qemu.d/win11/prepare/begin/
+ sudo cp revert.sh /etc/libvirt/hooks/qemu.d/win11/release/end/
 
- systemctl restart libvirtd
+ sudo systemctl restart libvirtd
